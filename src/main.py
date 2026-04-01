b533532e5032b4767ee506b542c0b1769a34bf69
khushiijais26-cmyk <khushiijais.26@gmail.com>
2026-04-02 00:21:26 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,20 +1,14 @@
 import cv2
 from camera.camera_input import CameraInput
-
-
-def main():
-    camera = CameraInput(0)
-
-    while True:
-        import cv2
-from camera.camera_input import CameraInput
 from processing.frame_processor import FrameProcessor
+from detection.people_detector import PeopleDetector
 
 
 def main():
 
     camera = CameraInput(0)
     processor = FrameProcessor()
+    detector = PeopleDetector()
 
     while True:
 
@@ -25,15 +19,27 @@ def main():
 
         processed_frame = processor.process(frame)
 
-        # Convert back for display
-        display_frame = cv2.cvtColor(processed_frame, cv2.COLOR_RGB2BGR)
+        detected_frame, count = detector.detect(processed_frame)
 
-        cv2.imshow("Smart Queue System", display_frame)
+        print("People count:", count)
+
+        cv2.putText(
+            detected_frame,
+            f"People Count: {count}",
+            (20, 40),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (0, 255, 0),
+            2
+        )
+
+        cv2.imshow("Smart Queue System", detected_frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
     camera.release()
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
