081f9c90c7cfee67b3818b077d47290e954652e5
pranjalsinha005-dotcom <pranjal.sinha005@gmail.com>
2026-04-04 12:48:08 +0530

    Update data_logger.py

--- src/data/data_logger.py
+++ src/data/data_logger.py
@@ -1,32 +1,25 @@
-import csv
+import pandas as pd
 import os
 from datetime import datetime
 
 
 class DataLogger:
 
-    def __init__(self, filename="queue_data.csv"):
-
-        self.filename = filename
+    def __init__(self):
+        self.file = "queue_data.csv"
 
         # Create file if not exists
-        if not os.path.exists(self.filename):
-            with open(self.filename, "w", newline="") as f:
-                writer = csv.writer(f)
-                writer.writerow([
-                    "Timestamp",
-                    "People_Count",
-                    "Queue_Status",
-                    "Waiting_Time"
-                ])
+        if not os.path.exists(self.file):
+            df = pd.DataFrame(columns=["Timestamp", "People", "Queue", "Waiting"])
+            df.to_csv(self.file, index=False)
 
-    def log(self, people, status, wait):
+    def log(self, people, queue, waiting):
+        data = {
+            "Timestamp": datetime.now(),
+            "People": people,
+            "Queue": queue,
+            "Waiting": waiting
+        }
 
-        with open(self.filename, "a", newline="") as f:
-            writer = csv.writer(f)
-            writer.writerow([
-                datetime.now().strftime("%H:%M:%S"),
-                people,
-                status,
-                wait
-            ])
+        df = pd.DataFrame([data])
+        df.to_csv(self.file, mode='a', header=False, index=False)
